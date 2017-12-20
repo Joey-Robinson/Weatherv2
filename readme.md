28e6cf626228efd74d09c0853205951487cb576a
Gosu <theamazingness@gmail.com>
2017-12-20 15:47:51 -0600

    first commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+---
+title: 'weatherapp'
+---
+new and improved weather app. use your own apikey from wunderground for this to work
---
+++ weather.js
@@ -0,0 +1,16 @@
+class Weather {
+  constructor(city, state) {
+    this.apiKey = '';
+    this.city = city;
+    this.state = state;
+  }
+  async getWeather() {
+    const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+    const responseData = await response.json();
+    return responseData.current_observation;
+  }
+  changeLocation(city, state) {
+    this.city = city;
+    this.state = state;
+  }
+}
